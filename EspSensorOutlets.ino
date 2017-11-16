62a4fa4dfa854b72437192139a5e8be3f290b57c
Robb Jones <robb.kc.jones@gmail.com>
2017-11-15 20:51:40 -0600

    mqtt tweaks

--- EspSensorOutlets.ino
+++ EspSensorOutlets.ino
@@ -7,10 +7,11 @@
 #include <PubSubClient.h>  /* https://pubsubclient.knolleary.net/api.html */
 #include "dht.h"
 
-#include <local-config.h>
+#include "local-config.h"
 
 /****************************************************************************************/
-#define MQTT_KEEPALIVE 15*60+10  /* override PubSubClient.h */
+#define MQTT_KEEPALIVE 5*60+10  // overrides PubSubClient.h
+#define MQTT_PUB_SPAN_MS 100  // publishing messages in rapid success seems to be problematic?
 const unsigned long ENV_UPDATE_SECS = MQTT_KEEPALIVE-10;
 #define DHT11_PIN 5
 const unsigned int OUTLET_PIN [] = {14, 4, 0, 2};
@@ -22,6 +23,11 @@ dht DHT;
 WiFiClient wifiClient;
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
 PubSubClient mqttClient(MQTT_HOST, MQTT_PORT, mqtt_callback, wifiClient);
+float lastPubTime = 0;
+// Humidity sensor reads low.  Until we know more about its (in)accuracy, performing a
+// crude correction to the value to known value.
+// nov 2017 reads 21, actual is 45
+#define DHT_HUMID_OFFSET 24
 /****************************************************************************************/
 
 /****************************************************************************************/
@@ -32,11 +38,25 @@ void setup() {
 
   for (int i=0; i<OUTLET_CT; i++) {
     pinMode(OUTLET_PIN[i], OUTPUT);
-    digitalWrite(OUTLET_PIN[i], LOW);
+    digitalWrite(OUTLET_PIN[i], HIGH);
   }
-  pinMode(BUILTIN_LED, OUTPUT);
 
   connectWifi();
+  connectMqtt();
+  delay(50);
+
+//// NOW: Publish off state for all outlets so any clients watching state are aware.
+////      We don't care if we aren't listening yet, this set of messages aren't for us :-)
+//// FUTURE: Save outlet values in EPROM so we can power up to last known state.
+  lastPubTime = millis();  // force a delay before we send - was having problems here
+  publishFloat(TOPIC_OUTLET1, 0);
+  mqttClient.loop();
+  publishFloat(TOPIC_OUTLET2, 0);
+  mqttClient.loop();
+  publishFloat(TOPIC_OUTLET3, 0);
+  mqttClient.loop();
+  publishFloat(TOPIC_OUTLET4, 0);
+  mqttClient.loop();
 }
 /****************************************************************************************/
 
@@ -44,24 +64,25 @@ void setup() {
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
-  Serial.print("] ");
+  Serial.print("] [");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  Serial.println();
-  if (2 != length) {
-    Serial.println("mqtt_callback: Bad length.");
-    return; 
-  }
+  Serial.println("]");
 
   // Switch on the LED if an 1 was received as first character
-  int i = ((int) payload[0]) - 48;
-  Serial.print("i=");
-  Serial.println(i);
-  if (0 < i && i <= OUTLET_CT) {  // starting at one b/c invalid data could potentiall present as zero
-      int val = ((int) payload[1]) - 48;
-      Serial.print("val=");
-      Serial.println(val);
+  int i = 0;
+  if (0 == strcmp(topic, TOPIC_OUTLET1)) {
+    i = 1;
+  } else if (0 == strcmp(topic, TOPIC_OUTLET2)) {
+    i = 2;
+  } else if (0 == strcmp(topic, TOPIC_OUTLET3)) {
+    i = 3;
+  } else if (0 == strcmp(topic, TOPIC_OUTLET4)) {
+    i = 4;
+  } 
+  if (0 < i && i <= OUTLET_CT) {  // starting at one b/c invalid data could potentially present as zero
+      int val = ((int) payload[0]) - 48;
       setOutlet(i-1, (val==1));
   }
 }
@@ -79,7 +100,7 @@ void setOutlet(unsigned int n, bool state) {
   Serial.print(n);
   Serial.print("/");
   Serial.print(OUTLET_PIN[n]);
-  Serial.print(", ");
+  Serial.print("): ");
   Serial.println((state) ? "LOW" : "HIGH");
 }
 
@@ -90,6 +111,10 @@ float ctof(float c) {
 
 
 void connectWifi() {
+  if (WiFi.status() == WL_CONNECTED) {
+    return;
+  }
+
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
@@ -114,12 +139,23 @@ void connectWifi() {
 
 void connectMqtt() {
   if (!mqttClient.connected()) {
+    Serial.print("Connecting to MQTT server.....");
     char willTopic[27];
     sprintf(willTopic, "/status/%s", MQTT_CLIENT_ID);
     const char *willMsg = "Disconnected";
     
     mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_P, willTopic, 0, 1, willMsg);
-    mqttClient.subscribe(TOPIC_OUTLET);
+    if (mqttClient.connected()) {
+      Serial.println("OK");
+    } else {
+      Serial.println("FAILED!");
+      Serial.print("MQTT state = ");
+      Serial.println(mqttClient.state()); // neg=lost connection, pos=never had one
+    }
+    mqttClient.subscribe(TOPIC_OUTLET1);
+    mqttClient.subscribe(TOPIC_OUTLET2);
+    mqttClient.subscribe(TOPIC_OUTLET3);
+    mqttClient.subscribe(TOPIC_OUTLET4);
   }
 }
 
@@ -152,29 +188,48 @@ void sendEnvReadings() {
     connectWifi();
   }
 
-  connectMqtt();
+  connectMqtt();  // leave the connection open for All of Time
 
   if (readDht11()) {
-    char payload[24];
-    
-    float temperature = ctof(DHT.temperature);
-    char sTemp[10];
-    dtostrf(temperature, 4, 2, sTemp);
-
-    float humidity = DHT.humidity;
-    char sHumidity[10];
-    dtostrf(humidity, 4, 2, sHumidity);
-    
-    sprintf(payload, "%s %s", sTemp, sHumidity);
-    Serial.println(payload);
-    Serial.print("MQTT client is connected?  ");
-    Serial.println(mqttClient.connected());
-    if (mqttClient.publish(TOPIC_ENV, payload)) {
-      Serial.println("Publish succeeded");
-    } else {
-      Serial.println("Publish FAILED!");
-      mqttClient.disconnect();
+    publishFloat(TOPIC_ENV_TEMP, ctof(DHT.temperature));
+    float h = DHT.humidity + DHT_HUMID_OFFSET;
+    if (h > 100) {
+      h = 99;
     }
+    publishFloat(TOPIC_ENV_HUMID, h);
+  }
+}
+    
+
+void publishFloat(const char * topic, const float val) {
+  char sval[10];
+  dtostrf(val, 4, 2, sval);
+  publishString(topic, sval);
+}
+
+void publishInt(const char * topic, const int val) {
+  char sval[10];
+  itoa(val, sval, 10);
+  publishString(topic, sval);
+}
+
+void publishString(const char * topic, const char * val) {
+  Serial.print("MQTT client is connected?  ");
+  Serial.println(mqttClient.connected());
+  Serial.print("Publish to topic [");
+  Serial.print(topic);
+  Serial.print("] value [");
+  Serial.print(val);
+  Serial.print("] ");
+  while (millis() < lastPubTime + MQTT_PUB_SPAN_MS) {
+    delay(20);
+  }
+  if (mqttClient.publish(topic, val)) {
+    lastPubTime = millis();
+    Serial.println("succeeded");
+  } else {
+    Serial.println("FAILED!");
+//    mqttClient.disconnect();
   }
 }
 
@@ -188,7 +243,7 @@ unsigned long timeToSendEnvReadings = 0;
 /****************************************************************************************/
 /****************************************************************************************/
 void loop() {
-  delay(500); // dht11 fastest poll frequency is 1 second.  This will also limit polling for outlet mqtt messages.
+//  delay(250);
   mqttClient.loop();
   if (millis() > timeToSendEnvReadings) {
     Serial.println("Time to update environment readings.....");
